68ae10a7c45ca7eb8c2297541d80a2b032a58bdd
Jerry Zhang <jerrybibo@hotmail.com>
2018-09-06 23:53:55 -0400

    Modified the README.md file Included instructions for git setup

---
+++ main.py
@@ -0,0 +1,10 @@
+# Here I'm adding a comment and making an empty function.
+# This is just to test that git works properly.
+
+
+def an_empty_function():
+    # aww. how sad.
+    pass
+
+
+an_empty_function()
