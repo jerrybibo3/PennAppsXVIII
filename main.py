8869ed7309be6dff33c0f792141cf573cf77d94d
Jerry Zhang <jerrybibo@hotmail.com>
2018-09-07 18:43:42 -0400

    Updated main.py to base Pygame code

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-# Here I'm adding a comment and making an empty function.
-# This is just to test that git works properly.
+import pygame as p
+import sys
+import math
+from pygame.locals import *
+
+WHITE = (255,) * 3
+BLACK = (0,) * 3
+
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+
+# TODO Declare constants here
+
+p.init()
+wS = p.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
 
 
-def an_empty_function():
-    # aww. how sad.
-    pass
+def main():
+    while 1:
+        # TODO Put your code here
+        wS.fill(BLACK)
+        for event in p.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    p.quit()
+                    sys.exit(0)
+            if event.type == QUIT:
+                p.quit()
+                sys.exit(0)
+        p.display.update()
 
 
-
-def another_empty_function():
-    pass
-
-
-# Here's a new commentasdassdd
-
-an_empty_function()
-
+main()
